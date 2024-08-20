1e6d2174f3f0246516f4d21db2c41c3bd004e3b1
aaronbanerjee123 <aaronbaner@outlook.com>
2024-08-20 17:21:34 -0400

    implemented the form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,103 +1,102 @@
-// 'use client'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
 
-// import Image from 'next/image'
-// import Link from 'next/link'
-// import { zodResolver } from "@hookform/resolvers/zod"
-// import { useForm } from "react-hook-form"
-// import React, { useState } from 'react'
-// import { z } from "zod"
-// import { Button } from "@/components/ui/button"
-// import {
-//     Form,
-//     FormControl,
-//     FormDescription,
-//     FormField,
-//     FormItem,
-//     FormLabel,
-//     FormMessage,
-// } from "@/components/ui/form"
-// import { Input } from "@/components/ui/input"
-// import CustomInput from './CustomInput'
-// import { authFormSchema } from '@/lib/utils'
-// import { Loader2 } from 'lucide-react'
+const AuthForm = ({ type }: { type: string }) => {
+  const [user, setUser] = useState(null);
 
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      username: "",
+    },
+  });
 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
-// const AuthForm = ({ type }: { type: string }) => {
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8">
+        <Link href="/" className="cursor-pointer items-center flex gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            Horizon
+          </h1>
+        </Link>
 
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          <p className="text-16 font-normal text-gray-600">
+            {user
+              ? "Link your account to get started"
+              : "Please enter your details"}
+          </p>
+        </div>
+      </header>
+      {user ? (
+        <div className="flex flex-col gap-4"></div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <CustomInput
+                control={form.control}
+                name="username"
+                label="Username"
+                placeholder="Enter your username"
+              />
+              <CustomInput
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your email"
+              />
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+              />
 
-//     const [user, setUser] = useState(null);
-//     const [isLoading, setIsLoading] = useState(false);
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </>
+      )}
+    </section>
+  );
+};
 
-
-//     const form = useForm<z.infer<typeof authFormSchema>>({
-//         resolver: zodResolver(authFormSchema),
-//         defaultValues: {
-//             email: "",
-//             password: ""
-//         },
-//     })
-
-//     // 2. Define a submit handler.
-//     function onSubmit(values: z.infer<typeof authFormSchema>) {
-//         // Do something with the form values.
-//         // ✅ This will be type-safe and validated.
-//         setIsLoading(true)
-//         console.log(values)
-//         setIsLoading(false)
-
-//     }
-
-
-//     return (
-//         <section className="auth-form">
-//             <header className="flex flex-col gap-5 md:gap-8">
-//                 <Link href="/" className="cursor-pointer items-center flex gap-1">
-//                     <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon logo"
-//                         className="size-[24px] max-xl:size-14" />
-//                     <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
-//                 </Link>
-
-
-//                 <div className="flex flex-col gap-1 md:gap-3">
-//                     <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
-//                         {user ? 'Link Account' : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
-//                     </h1>
-
-//                     <p className="text-16 font-normal text-gray-600">
-//                         {user ? 'Link your account to get Started' : 'Please enter your details'}
-//                     </p>
-//                 </div>
-//             </header>
-
-//             {user ? (
-//                 <div className="flex flex-col gap-4"></div>
-//             ) : <>
-
-//                 <Form {...form}>
-//                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-//                         <CustomInput control={form.control} name='email' label='Username' placeholder='Enter your username' />
-
-//                         <CustomInput control={form.control} name='password' label='Password' placeholder='Enter your password' />
-
-
-
-//                         <Button type="submit" className="form-btn">
-//                             {isLoading ? (
-//                                 <>
-//                                 <Loader2 size={20} className="animate-spin" /> &nbsp; Loading ....
-//                                 <>
-//                             ):
-//                             (
-
-//                             )
-//                             )}
-//                         </Button>
-//                     </form>
-//                 </Form>
-//             </>}
-//         </section>
-//     )
-// }
-
-// export default AuthForm
+export default AuthForm;
